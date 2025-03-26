21b3062fbe1186d9215228fd2bc61080eed720af
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-26 18:38:19 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,5 +1,5 @@
+from re import *
 s = open('24 (8).txt').readline()
-s = s.replace('CA','0')
-for i in range(1,1000):
-    if '0'*i in s:
-        print(i)
+a = r'([CA]+)+C?'
+k = [len(x) for x in findall(a,s)]
+print(max(k))
