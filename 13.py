242e046cbbce9444d916cf54385d513022457430
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-26 19:04:49 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,18 +1,17 @@
-s = open('24 (4).txt').readline()
-s = 'AFCDOFAOFCFAFCFADCOFADOAFC'
+s = open('24 (12).txt').readline()
+#s = 'AFCDOFAOFCFAFCFADCOFADOAFC'
 p = ''
 mxp = ''
+for k in 'AO':
+    s = s.replace(k, '+')
+for l in 'CDF':
+    s = s.replace(l, '-')
 for i in s:
     p += i
     p1 = p
-    for k in 'AO':
-        p1 = p1.replace(k,'+')
-    for l in 'CDF':
-        p1 = p1.replace(k, '-')
     if '-++-' not in p:
         if len(p) > len(mxp):
             mxp = p
     else:
         p = p[-3:]
 print(len(mxp))
- 
