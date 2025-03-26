79fe484b45d2147e3215ef81be4a4d86af1b0bce
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-26 18:00:44 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,8 @@
-s = open('24 (3).txt').readline()
+s = open('24 (2).txt').readline()
 #s = 'XXXXYYZXYZYXZXXXXYYZ'
-print(s.count('XXXX'))
+k = 0
+for i in range(0,len(s)-4):
+    if s[i]+s[i+1]+s[i+2]+s[i+3] == 'XXXX':
+        k+=1
+print(k)
+
